03e775f3555f279b8225e8aa6253f1528f15edea
AnastasiaCymbalyuk <n.wolk@mail.ru>
2021-01-21 08:06:51 +0300

    задание 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,30 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
-}
+let arr = [{name: "percent", value: percent}, {name: "contribution", value: contribution}, {name: "amount", value: amount}, {name: "date", value: date}];
+for(let i = 0; i < arr.length; i++){
+	if(isNaN(arr[i].value)){
+		console.log(`Параметр ${arr[i].name} содержит неправильное значение ${arr[i].value}`);
+	} else {
+		Number(arr[i].value);
+	};
+};
+let sum = amount - contribution;
+let month = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+let interestRate = (percent / 100) / 12;
+let monthlyPayment = (sum * (interestRate + interestRate / (((1 + interestRate) ** month) - 1)));
+let totalAmount = parseFloat((monthlyPayment * month).toFixed(2));
+console.log(totalAmount);
+return totalAmount;
+};
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+if(!name){
+	name = "Аноним";
+}
+let greeting = `Привет, мир! Меня зовут ${name}`;
+console.log(greeting);
+return greeting;
 }
